b3283d412c8dc88c0d99373215396cf287431a61
Петров Алексей Михаилович <alexey.petrov@kaspi.kz>
2025-06-11 16:10:35 +0500

    doc: set package info

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,14 +6,14 @@ export default defineNuxtConfig({
   prometheus: {
     prefix: 'playground_',
     verbose: true,
-    // clusterPort: 9100,
+    clusterPort: 9100,
   },
-  // nitro: {
-  //   preset: 'node-cluster',
-  //   options: {
-  //     cluster: {
-  //       workers: '1' // or a specific number
-  //     }
-  //   }
-  // }
+  nitro: {
+    preset: 'node-cluster',
+    // options: {
+    //   cluster: {
+    //     workers: '1' // or a specific number
+    //   }
+    // }
+  }
 })
